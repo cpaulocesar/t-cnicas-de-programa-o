5fc102708c2e3cba07a27ee842a0183388e2e1f0
cpaulocesar <160556974+cpaulocesar@users.noreply.github.com>
2024-09-11 23:56:38 -0300

    Add files via upload

---
+++ tp series/Filmes-API-Spring-TP2-master/src/test/java/br/edu/fatecpg/filmes/FilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fatecpg.filmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
